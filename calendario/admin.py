ccbf84bd356d9f1993d069f84800e8131fbbbfdc
superqwe <superqwe@users.noreply.github.com>
2026-03-18 22:14:06 +0100

    admin.py

--- calendario/admin.py
+++ calendario/admin.py
@@ -5,8 +5,9 @@ from .models import Evento, Campo, Presenza, Attivita
 
 @admin.register(Evento)
 class EventoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'data'
     list_display = ('data', 'conferma', 'orario', 'attivita', 'campo', 'note')
-    list_filter = ('attivita',)
+    list_filter = ('conferma', 'attivita')
     ordering = ('-data',)
     search_fields = ('data',)
 
@@ -15,7 +16,7 @@ class EventoAdmin(admin.ModelAdmin):
 class PresenzaAdmin(admin.ModelAdmin):
     date_hierarchy = 'evento__data'
     list_display = ('evento__data', 'evento__attivita', 'evento__campo', 'utente__nickname', 'risposta')
-    list_filter = ('utente', 'risposta')
+    list_filter = ('risposta', 'utente',)
     ordering = ('-evento__data', 'risposta', 'utente__nickname')
 
 
