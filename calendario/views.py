f36ac0717599f7818d368dca29771aa87c39aebb
superqwe <superqwe@users.noreply.github.com>
2026-03-24 23:10:53 +0100

    evento.html: formato conferma

--- calendario/views.py
+++ calendario/views.py
@@ -1,8 +1,7 @@
 import datetime
 
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 
 from .models import Evento, Presenza
 from .wh import invia_messaggio
