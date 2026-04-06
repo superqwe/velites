171e1d7ccc9f7f62c50db9b4c903e63dd7bb7489
superqwe <superqwe@users.noreply.github.com>
2026-04-06 22:00:55 +0200

    inizio calendario_menu_preferenze.html

--- calendario/views.py
+++ calendario/views.py
@@ -14,16 +14,20 @@ def prossimo_evento_id():
 
 def eventi_futuri(request):
     eventi_futuri = Evento.objects.filter(data__gte=datetime.date.today()).order_by('data')
-    context = {'eventi': eventi_futuri,
-               'pagina_attiva_eventi_futuri': 'active'}
+    context = {
+        'titolo': 'Calendario',
+        'eventi': eventi_futuri,
+        'pagina_attiva_eventi_futuri': 'active'}
 
     return render(request, 'calendario/elenco_eventi.html', context)
 
 
 def eventi_passati(request):
     eventi_passati = Evento.objects.filter(data__lt=datetime.date.today()).order_by('-data')
-    context = {'eventi': eventi_passati,
-               'pagina_attiva_eventi_passati': 'active'}
+    context = {
+        'titolo': 'Storico',
+        'eventi': eventi_passati,
+        'pagina_attiva_eventi_passati': 'active'}
 
     return render(request, 'calendario/elenco_eventi.html', context)
 
@@ -32,41 +36,6 @@ def prossimo_evento(request):
     request.session['prossimo'] = 'active'
 
     return redirect('calendario:evento', id=prossimo_evento_id())
-
-
-def evento2(request, id):
-    # todo: obsoleto --> cancellare
-    utente = request.user
-    evento = get_object_or_404(Evento, pk=id)
-    presenze = evento.partecipazioni.all().order_by('utente__username')
-    messaggio_ok = False
-
-    if request.method == 'POST':
-        presenza_id = request.POST.get('salva')
-        nuova_risposta = request.POST.get(f'risposta_{presenza_id}')
-        nuova_nota = request.POST.get(f'nota_{presenza_id}')
-
-        presenza = get_object_or_404(Presenza, pk=presenza_id)
-        presenza.risposta = nuova_risposta
-        presenza.nota = nuova_nota
-        presenza.save()
-
-        if prossimo_evento_id() == evento.pk:
-            invia_messaggio(evento, presenze)
-
-        messaggio_ok = True
-
-    context = {
-        "evento": evento,
-        'utente': utente,
-        "presenze": presenze,
-        "messaggio_ok": messaggio_ok,
-    }
-
-    if prossimo_evento_id() == evento.pk:
-        context["pagina_attiva_prossimo_evento"] = 'active'
-
-    return render(request, "calendario/evento.html", context)
 
 
 def evento(request, id):
